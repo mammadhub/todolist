4a0f0accfdc4928672fcf9d96a3099e2266550e9
mammad <memmed2512@gmail.com>
2024-05-03 11:02:24 +0400

    todolist

---
+++ script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector("#add");
+const btn = document.querySelector(".topla");
+const ul = document.querySelector(".create");
+const todo = document.querySelector("#todo");
+
+function ElaveEt() {
+    const todoMetin = input.value;
+    if (todoMetin.trim() !== "") { 
+        input.style.border = "2px solid black";
+        const li = document.createElement("li");
+        li.textContent = todoMetin;
+        ul.append(li);
+        input.value = "";
+    }
+    else{
+        input.style.border = "2px solid red";
+    }
+ 
+}
+function clear() {
+    ul.innerHTML="";
+}
